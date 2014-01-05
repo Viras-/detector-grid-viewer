d537ed5e54ef26ac3a5b2b85f5a30fb0566b4292
Viras- <viras@users.sourceforge.net>
2014-01-05 08:16:17 +0100

    sending of requests to spread communication layer is now possible

--- protected/config/main.sample.php
+++ protected/config/main.sample.php
@@ -80,5 +80,6 @@ return array(
     'params' => array(
         // this is used in contact page
         'adminEmail' => 'webmaster@example.com',
+        'spreadDaemon' => '4803@localhost',
     ),
 );
